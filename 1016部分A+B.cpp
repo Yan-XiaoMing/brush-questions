e816cf85deadf1ba54aaaeb7d4f3e429b9012463
YanQiMing <1024744235@qq.com>
2020-07-07 00:08:24 +0800

    完成pat乙级1016-1017

---
+++ 1016部分A+B.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+/*
+int main() {
+	int a, da, b, db;
+	int tmp1, tmp2,count1,count2;
+	int sum1, sum2, sum;
+	sum1 = sum2 = sum = 0;
+	count1 = count2 = 0;
+
+	scanf("%d %d %d %d", &a, &da, &b, &db);
+	while (a){
+		int t = a % 10;
+		a /= 10;
+		if (t == da) {
+			count1++;
+		}
+	}
+	while (b) {
+		int t = b % 10;
+		b /= 10;
+		if (t == db) {
+			count2++;
+		}
+	}
+
+	//printf("%d %d %d %d\n", da, db, count1, count2);
+
+	for (int i = 0; i < count1; i++) {
+		sum1 += da * pow(10, i);
+	}
+	//printf("%d\n", sum1);
+
+	for (int i = 0; i < count2; i++) {
+		sum2 += db * pow(10, i);
+	}
+	//printf("%d\n", sum2);
+
+	sum = sum1 + sum2;
+	printf("%d\n", sum);
+	return 0;
+
+}
+*/
