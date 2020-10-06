4ac6b6137e03cff5ebdf76a8e488461c3ba84acb
PaperGangsta <1024744235@qq.com>
2020-10-06 22:48:23 +0800

    常规提交

---
+++ pat/advance/1001A+B Format.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main() {
+//    int a, b,sum;
+//    cin >> a >> b;
+//    sum = a + b;
+//    if (sum < 0) {
+//        cout << "-";
+//        sum = -sum;
+//    }
+//    string s = to_string(sum);
+//    int len = s.length();
+//    for (int i = 0; i < len; i++) {
+//        cout << s[i];
+//        if ((i+1) % 3 == len % 3 && i!=len-1) {
+//            cout << ",";
+//        }
+//    }
+//    return 0;
+//}
