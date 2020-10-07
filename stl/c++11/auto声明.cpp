3c74625b30147eb2123f813f8d301d3d63d3c16d
PaperGangsta <1024744235@qq.com>
2020-10-07 14:38:38 +0800

    常规提交

---
+++ stl/c++11/auto声明.cpp
@@ -0,0 +1,14 @@
+//#include<iostream>
+//#include<set>
+//using namespace std;
+//int main() {
+//	auto x = 100;//x为int变量
+//	auto y = 1.5;//y为double变量
+//	set<int> s;
+//	for (set<int>::iterator it = s.begin(); it != s.end(); it++) {
+//		cout << *it << endl;
+//	}
+//	for (auto it = s.begin(); it != s.end(); it++) {
+//		cout << *it << " ";
+//	}
+//}
---
+++ stl/c++11/for循环的新特性.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//int main() {
+//	int arr[4] = { 0,1,2,3 };
+//	vector<int>v;
+//	for (int i : arr) {
+//		cout << i << endl;
+//	}
+//
+//	for (int& i : arr) {
+//		i = i * 2;
+//	}
+//
+//	for (auto i : v) {
+//		cout << i << " ";
+//	}
+//	for (int i = 0; i < v.size(); i++) {
+//		cout << v[i] << " ";
+//	}
+//
+//	//arr[4]  = { 0,2,4,6 }
+//}
