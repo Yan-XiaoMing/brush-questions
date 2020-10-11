38e9fd3087f76471fc6c1edec19641c933fc741e
PaperGangsta <1024744235@qq.com>
2020-10-11 21:59:50 +0800

    常规提交

--- leetCode/探索/二叉树/已知前中转后.cpp
+++ leetCode/探索/二叉树/已知前中转后.cpp
@@ -4,15 +4,26 @@ using namespace std;
 int pre[] = { 1,2,3,4,5,6 };
 int in[] = { 3,2,4,1,6,5 };
 
+//void post(int root, int start, int end) {
+//	if (start > end) {
+//		return;
+//	}
+//	int i = start;
+//	while (i < end && in[i] != pre[root]) i++;
+//	post(root+1,start,i-1);
+//	post(root-start+i+1,i+1,end);
+//	printf("%d ", pre[root]);
+//}
+
 void post(int root, int start, int end) {
 	if (start > end) {
 		return;
 	}
 	int i = start;
 	while (i < end && in[i] != pre[root]) i++;
-	post(root+1,start,i-1);
-	post(root-start+i+1,i+1,end);
-	printf("%d ", pre[root]);
+	post(root+1,0,i-1); //×ó
+	post(root - start + i + 1,i+1,end); //ÓÒ
+	printf("%d", pre[root]);
 }
 
 int main() {
