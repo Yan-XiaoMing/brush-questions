757cc65a92bb5b169195f2f8f12cb025837742bc
PaperGangsta <1024744235@qq.com>
2020-10-11 21:51:02 +0800

    常规提交

---
+++ leetCode/探索/二叉树/已知前中转后.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int pre[] = { 1,2,3,4,5,6 };
+int in[] = { 3,2,4,1,6,5 };
+
+void post(int root, int start, int end) {
+	if (start > end) {
+		return;
+	}
+	int i = start;
+	while (i < end && in[i] != pre[root]) i++;
+	post(root+1,start,i-1);
+	post(root-start+i+1,i+1,end);
+	printf("%d ", pre[root]);
+}
+
+int main() {
+	post(0, 0, 5);
+	return 0;
+}
---
+++ pat/advance/1020 Tree Traversals.cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//#include<vector>
+//#include<queue>
+//#include<stack>
+//using namespace std;
+//int main() {
+//	int n;
+//	scanf("%d", &n);
+//	int inorder[31],postorder[31];
+//	for (int i = 0; i < n; i++) {
+//		scanf("%d", &postorder[i]);
+//	}
+//	for (int i = 0; i < n; i++) {
+//		scanf("%d", &inorder[i]);
+//	}
+//
+//}
