441c79a3814dbb106a0282cc7865b34e770ac9e3
PaperGangsta <1024744235@qq.com>
2021-02-03 11:35:20 +0800

    提交

---
+++ leetCode/15. 三数之和.js
@@ -0,0 +1,70 @@
+/**
+ * 双指针
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+let nums = [-1, 0, 1, 2, -1, -4];
+var threeSum = function (nums) {
+  let ans = [];
+  const len = nums.length;
+  if (nums == null || len < 3) return ans;
+  nums.sort((a, b) => a - b); // 排序
+  for (let i = 0; i < len; i++) {
+    if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+    if (i > 0 && nums[i] == nums[i - 1]) continue; // 去重
+    let L = i + 1;
+    let R = len - 1;
+    while (L < R) {
+      const sum = nums[i] + nums[L] + nums[R];
+      if (sum == 0) {
+        ans.push([nums[i], nums[L], nums[R]]);
+        while (L < R && nums[L] == nums[L + 1]) L++; // 去重
+        while (L < R && nums[R] == nums[R - 1]) R--; // 去重
+        L++;
+        R--;
+      } else if (sum < 0) L++;
+      else if (sum > 0) R--;
+    }
+  }
+  return ans;
+};
+var threeSum = function (nums) {
+  let res = [];
+  const length = nums.length;
+  if (nums === null || length < 3) {
+    return res;
+  }
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < length; i++) {
+      if(nums[i]>0){
+          break
+      }
+      if(i>0 && nums[i] === nums[i-1]){
+          continue
+      }
+      let left = i+1
+      let right = length - 1
+      while(left < right){
+          const sum = nums[left] + nums[right] + nums[i]
+          if(sum === 0){
+              res.push([nums[left],nums[right],nums[i]])
+              while(left < right && nums[left] === nums[left+1]){
+                  left++
+              }
+              while(left < right && nums[right] === nums[right-1]){
+                  right--
+              }
+              left++
+              right--
+          }
+          else if(sum > 0){
+              right--
+          }
+          else if(sum<0){
+              left++
+          }
+      }
+  }
+  return res
+};
+threeSum(nums);
