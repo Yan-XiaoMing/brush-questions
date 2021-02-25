210eb26aadde46d6e38c581b5c97e8e8bfa18047
yanqiming5 <yanqiming5@jd.com>
2021-02-25 21:40:56 +0800

    常规提交

---
+++ leetCode/探索/二叉树/二叉树上两点的距离.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} p
+ * @param {number} q
+ * @return {number}
+ */
+var distanceInTree = function(root, p, q) {
+
+};
+
+//最近公共祖先
+function lca(root,p,q){
+    
+}
