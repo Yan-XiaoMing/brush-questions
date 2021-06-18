9495be0a99625516975f7616b39d3ecd0abde024
PaperGangsta <1024744235@qq.com>
2021-06-18 20:40:57 +0800

    commit

--- leetCode/探索/剑指offer/剑指 Offer 13. 机器人的运动范围.js
+++ leetCode/探索/剑指offer/剑指 Offer 13. 机器人的运动范围.js
@@ -35,3 +35,48 @@ var movingCount = function (m, n, k) {
   }
   return s.size;
 };
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+
+const directionArr = [
+  [0, 1],
+  [1, 0],
+];
+function getSum(number) {
+  let sum = 0;
+  while (number) {
+    let num = number % 10;
+    number /= 10;
+    sum += num;
+  }
+  return sum;
+}
+
+var movingCount = function (m, n, k) {
+  let s = new Set(["0,0"]);
+  function dfs(x, y, k) {
+    for (let i = 0; i < 2; i++) {
+      let offsetX = x + directionArr[i][0];
+      let offsetY = y + directionArr[i][1];
+      if (
+        offsetX < 0 ||
+        offsetY < 0 ||
+        offsetX > m - 1 ||
+        offsetY > n - 1 ||
+        getSum(offsetX) + getSum(offsetY) > k ||
+        s.has(`${offsetX},${offsetY}`)
+      ) {
+        continue;
+      }
+      s.add(`${offsetX},${offsetY}`);
+      dfs(offsetX, offsetY, k);
+    }
+  }
+  dfs(0, 0, k);
+  return s.size;
+};
