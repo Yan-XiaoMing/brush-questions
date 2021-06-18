1957f6c190479284d64cc411f2141ec187e8a990
晔明 <yanqiming.yqm@alibaba-inc.com>
2021-06-18 20:40:57 +0800

    commit

--- leetCode/探索/剑指offer/剑指 Offer 14- I. 剪绳子.js
+++ leetCode/探索/剑指offer/剑指 Offer 14- I. 剪绳子.js
@@ -2,12 +2,26 @@
  * @param {number} n
  * @return {number}
  */
- var cuttingRope = function(n) {
-    let dp = []
-    dp[0] = 0
-    dp[1] = 1
-    dp[2] = 1
-    for(let i = 3;i<n+1;i++){
-
+var cuttingRope = function (n) {
+  let dp = new Array(n + 1).fill(1);
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 1;
+  for (let i = 3; i < n + 1; i++) {
+    for (let j = 2; j < i; j++) {
+      dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
     }
+  }
+  return dp[n];
+};
+
+var cuttingRope = function (n) {
+  let dp = new Array(n + 1).fill(1);
+  dp[2] = 1;
+  for (let i = 3; i < n + 1; i++) {
+    for (let j = 2; j < i; j++) {
+      dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+    }
+  }
+  return dp[n];
 };
---
+++ leetCode/探索/深度优先搜索/二叉树先序遍历.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  let result = [];
+  preorder(root, result);
+  return result;
+};
+
+function preorder(root, result) {
+  if (!root) {
+    return;
+  }
+  result.push(root.val);
+  preorder(root.left, result);
+  preorder(root.right, result);
+}
