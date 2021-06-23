8260e9c01e5d0f5b42d2c09d94cf7c6dc14baece
晔明 <yanqiming.yqm@alibaba-inc.com>
2021-06-24 01:47:20 +0800

    常规提交

--- leetCode/探索/剑指offer/剑指 Offer 38. 字符串的排列.js
+++ leetCode/探索/剑指offer/剑指 Offer 38. 字符串的排列.js
@@ -3,7 +3,21 @@
  * @return {string[]}
  */
 var permutation = function (s) {
-  if (!s) {
-    return [];
+  const res = new Set();
+  const visit = {};
+  function dfs(path) {
+    if (path.length === s.length) {
+      return res.add(path);
+    }
+    for (let i = 0; i < s.length; i++) {
+      if (visit[i]) {
+        continue;
+      }
+      visit[i] = true;
+      dfs(path + s[i]);
+      visit[i] = false;
+    }
   }
+  dfs("");
+  return [...res];
 };
