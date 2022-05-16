93c4f04e3c3c3ff3f4b96c70f657ce5dbc840316
yeming <1024744235@qq.com>
2022-05-16 09:11:55 +0800

    feat:增加34

--- leetCode/34. 在排序数组中查找元素的第一个和最后一个位置 .js
+++ leetCode/34. 在排序数组中查找元素的第一个和最后一个位置 .js
@@ -4,23 +4,38 @@
  * @return {number[]}
  */
 var searchRange = function (nums, target) {
-  const len = nums.length;
-  let left = 0,
-    right = len - 1;
+  let left = binarySearch(nums, target, "left");
+  let right = binarySearch(nums, target, "right");
+  return [left, right];
 };
 
-function binarySearch(nums, target, low) {
+function binarySearch(nums = [], target, boundary) {
   let left = 0,
-    right = nums.length - 1,
-    ans = nums.length;
+    right = nums.length - 1;
   while (left <= right) {
     let mid = left + Math.floor((right - left) / 2);
-    if (nums[mid] > target || (low && nums[mid] >= target)) {
-      right = mid - 1;
-      ans = mid;
-    } else {
+    if (nums[mid] === target) {
+      if (boundary === "left") {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else if (nums[mid] < target) {
       left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
     }
   }
-  return ans;
+  if (boundary === 'left') {
+    if (left >= nums.length || nums[left] !== target) {
+      return -1;
+    }
+    return left;
+  } else {
+    if (right < 0 || nums[right] !== target) {
+      return -1;
+    }
+    return right;
+  }
 }
+searchRange([5, 7, 7, 8, 8, 10], 8);
